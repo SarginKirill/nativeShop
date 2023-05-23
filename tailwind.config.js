df7f1b126f7d4a0c4853a887465156e2b473371c
SarginKirill <sarginkirill@gmail.com>
2023-05-23 10:21:19 +0300

    initial and config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        lightSand: '#FCF7E6',
+        dark: '#000',
+        white: '#fff',
+        orange: '#ED5E21',
+      },
+      fontFamily: {
+        sans: ['"Space Grotesk"', 'sans-serif'],
+        mono: ['"Space Mono"', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
